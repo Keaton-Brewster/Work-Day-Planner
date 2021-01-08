33585c536654cd158306b7802e110083095c7283
Delta9k8b <keaton.brewster@gmail.com>
2021-01-08 17:01:37 -0600

    changed the way the blocks update to show past, present, future

--- script.js
+++ script.js
@@ -19,22 +19,20 @@ $(document).ready(function () {
 
     // this interval updates the todo list every second so that it is always accurate as to which hour is past, present, or future
     function checkTime(box, hourStart, hourEnd) {
-        setInterval(() => {
-            if (
-                moment().isAfter(moment(hourStart, timeFormat)) && !moment().isBefore(moment(hourEnd, timeFormat))
-            ) {
-                box.addClass("past");
-            }
-            else if (
-                moment().isAfter(moment(hourStart, timeFormat)) && moment().isBefore(moment(hourEnd, timeFormat))
-            ) {
-                box.addClass("present");
-            } else if (
-                !moment().isAfter(moment(hourStart, timeFormat)) && moment().isBefore(moment(hourEnd, timeFormat))
-            ) {
-                box.addClass("future")
-            }
-        }, 1000);
+        if (
+            moment().isAfter(moment(hourStart, timeFormat)) && !moment().isBefore(moment(hourEnd, timeFormat))
+        ) {
+            box.addClass("past");
+        }
+        else if (
+            moment().isAfter(moment(hourStart, timeFormat)) && moment().isBefore(moment(hourEnd, timeFormat))
+        ) {
+            box.addClass("present");
+        } else if (
+            !moment().isAfter(moment(hourStart, timeFormat)) && moment().isBefore(moment(hourEnd, timeFormat))
+        ) {
+            box.addClass("future")
+        }
     };
 
 
@@ -73,14 +71,15 @@ $(document).ready(function () {
         timedisplay.html(moment().format('MMMM Do YYYY, h:mm:ss a'));
     }, 1000);
 
-
-    checkTime(nine, '09:00:00', '10:00:00');
-    checkTime(ten, '10:00:00', '11:00:00');
-    checkTime(eleven, '11:00:00', '12:00:00');
-    checkTime(twelve, '12:00:00', '13:00:00');
-    checkTime(one, '13:00:00', '14:00:00');
-    checkTime(two, '14:00:00', '15:00:00');
-    checkTime(three, '15:00:00', '16:00:00');
-    checkTime(four, '16:00:00', '17:00:00');
-    checkTime(five, '17:00:00', '18:00:00');
+    setInterval(() => {
+        checkTime(nine, '09:00:00', '10:00:00');
+        checkTime(ten, '10:00:00', '11:00:00');
+        checkTime(eleven, '11:00:00', '12:00:00');
+        checkTime(twelve, '12:00:00', '13:00:00');
+        checkTime(one, '13:00:00', '14:00:00');
+        checkTime(two, '14:00:00', '15:00:00');
+        checkTime(three, '15:00:00', '16:00:00');
+        checkTime(four, '16:00:00', '17:00:00');
+        checkTime(five, '17:00:00', '18:00:00');
+    }, 200;
 });
